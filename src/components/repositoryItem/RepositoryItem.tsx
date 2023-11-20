2d26eb1e6fa92a602aecaec6e669db122d9dfdd6
Adam El Amrani Colina <adelamco1091@outlook.com>
2023-11-20 23:29:57 +0100

    Create RepositoryItem test. Pass coverage at 100%

--- src/components/repositoryItem/RepositoryItem.tsx
+++ src/components/repositoryItem/RepositoryItem.tsx
@@ -2,12 +2,12 @@ import { RepositoryItemProps } from "./RepositoryInterface";
 
 const RepositoryItem = ({ repository }: RepositoryItemProps): JSX.Element => {
   return (
-    <div className="repository-item">
+    <li className="repository-item">
       <p>
         <strong>{repository.name}</strong> {repository.description}
         <a href={repository.html_url}>Access Repository</a>
       </p>
-    </div>
+    </li>
   );
 };
 
