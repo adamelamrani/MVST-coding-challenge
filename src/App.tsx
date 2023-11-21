7407ee7c3c2c1ff1318f09c258ef658b91ae8bb9
Adam El Amrani Colina <adam@adamelamrani.com>
2023-11-21 13:22:04 +0100

    Add search query params

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { toastSelector } from "./utils/toasts/toastSelector";
 import { ToastTypeEnum } from "./utils/toasts/ToastTypeEnum";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
 import { Repository } from "./components/repositoryItem/RepositoryInterface";
 
@@ -36,11 +36,12 @@ const GET_REPOSITORIES = gql`
 function App() {
   const [username, setUsername] = useState<string | null>();
   const debouncedUsername = useDebounce(username, 500);
+  const params = window.location.search.replace("?query=", "");
   const { loading, error, data } = useQuery(GET_REPOSITORIES, {
     variables: {
-      username: debouncedUsername[0],
+      username: debouncedUsername[0] || params,
     },
-    skip: !debouncedUsername[0],
+    skip: !debouncedUsername[0] && !params,
   });
 
   if (error) {
@@ -51,6 +52,15 @@ function App() {
   }
   const repositories: Repository[] = data?.user.repositories.nodes;
 
+  useEffect(() => {
+    if (debouncedUsername[0]) {
+      const query = new URLSearchParams(window.location.search);
+      query.set("query", encodeURIComponent(debouncedUsername[0]));
+
+      window.history.replaceState({}, "", `${location.pathname}?${query}`);
+    }
+  }, [debouncedUsername]);
+
   return (
     <>
       <p>MVST - Work in Progress!</p>
@@ -59,22 +69,22 @@ function App() {
         onChange={({ target }) => setUsername(target.value)}
         placeholder="username"
       />
-      {username && <h1>Repositories from {username}</h1>}
+      {username && <h1>Repositories from {debouncedUsername[0]}</h1>}
       {!username && !data && !error && (
         <p>
           <strong>Type a username to search for repositories!</strong>
         </p>
       )}
+      {loading && !data && (
+        <p>
+          <strong>Loading...</strong>
+        </p>
+      )}
+      {error && <p>{error.message}</p>}
       <RepositoriesList>
-        {loading && !data && (
-          <p>
-            <strong>Loading...</strong>
-          </p>
-        )}
         {repositories?.map((repository: Repository) => {
           return <RepositoryItem key={repository.id} repository={repository} />;
         })}
-        {error && <p>{error.message}</p>}
       </RepositoriesList>
       <ToastContainer />
     </>
