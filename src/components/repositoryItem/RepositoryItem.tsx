b0a7f37bdb6c6be7e73d89874d2122ce3f52e567
Adam El Amrani Colina <adam@adamelamrani.com>
2023-11-23 21:06:26 +0100

    Add extra properties to RepositoryItem list item

--- src/components/repositoryItem/RepositoryItem.tsx
+++ src/components/repositoryItem/RepositoryItem.tsx
@@ -5,12 +5,31 @@ const RepositoryItem = ({ repository }: RepositoryItemProps): JSX.Element => {
   console.log(repository);
   return (
     <li className={RepositoryStyle.repositoryItem}>
-      <p className={RepositoryStyle.repositoryParagraph}>
-        <strong>{repository.name}</strong> {repository.description}
-        <a className={RepositoryStyle.anchorStyle} href={repository.url}>
-          Access Repository
-        </a>
-      </p>
+      <header className={RepositoryStyle.repositoryItemHeader}>
+        <h3 className={RepositoryStyle.itemHeading}>{repository.name}</h3>
+        <div>
+          <span className={RepositoryStyle.starIcon}>⭐</span>
+          <span>{repository?.stargazers.totalCount}</span>
+        </div>
+      </header>
+      <div>
+        {repository?.description && (
+          <p className={RepositoryStyle.repositoryParagraph}>
+            {repository.description}
+          </p>
+        )}
+
+        {repository?.languages?.nodes.length > 0 && (
+          <ul className={RepositoryStyle.languagesList}>
+            {repository.languages.nodes.map((language) => (
+              <li key={language.name}>{language.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <a className={RepositoryStyle.anchorStyle} href={repository.url}>
+        Access Repository
+      </a>
     </li>
   );
 };
